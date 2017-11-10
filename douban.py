e83b3b49fa091af77dde4e289bbf01818277b9dd
wangzhiwen <wzw15517028333@gmail.com>
2017-11-10 17:32:54 +0800

    Add more posts.

--- douban.py
+++ douban.py
@@ -10,7 +10,11 @@ headers = {
 }
 
 # 帖子的连接
-bash_url = ''
+bash_urls = [
+    '',
+    '',
+    '',
+]
 
 def login(username,passwd,url='https://accounts.douban.com/login'):
     '''
@@ -66,7 +70,7 @@ def verification_code(url):
     else:
         return caprcha_id,ver_code_link
 
-def get_group_url():
+def get_group_url(bash_url):
     '''
     得到小组的链接，判断是否加入小组。
     :return: group_url
@@ -75,13 +79,13 @@ def get_group_url():
     group_url = bsobj.find('div',{'class':'title'}).find('a')['href']
     return group_url
 
-def join_the_group(session):
+def join_the_group(session,bash_url):
     '''
     判断是否加入这个小组，未加入则无法正常回帖!
     :param group_url: 小组连接
     :return:
     '''
-    group_url = get_group_url()
+    group_url = get_group_url(bash_url)
     bsobj = BeautifulSoup(session.get(group_url).text,'lxml')
     try:
         # 未加入小组
@@ -97,75 +101,82 @@ def join_the_group(session):
             session.get(join_group['href'])
             print('你已经加入小组')
 
-def replies(session):
+def replies(session,bash_url):
     '''
     顶贴 参考 http://xhzyxed.cn/2017/11/07/%E8%B1%86%E7%93%A3%E8%87%AA%E5%8A%A8%E9%A1%B6%E8%B4%B4/#more
     :param session:
     :return:
     '''
+    print('正在回帖%s'%bash_url)
     url = bash_url+'add_comment'
+    ##在控制台查看顶贴的次数
+    ##提交的数据
+    ding_text = requests.get('http://whatthecommit.com/index.txt').text
+    print(ding_text)
+    data = {
+        "rv_comment": ding_text,
+        "ck": "aubV",
+        'start': '0',
+        'submit_btn': '加上去'
+    }
     while True:
-        ##在控制台查看顶贴的次数
-        ##提交的数据
-        ding_text = requests.get('http://whatthecommit.com/index.txt').text
-        print(ding_text)
-        data = {
-            "rv_comment": ding_text,
-            "ck": "aubV",
-            'start': '0',
-            'submit_btn': '加上去'
-        }
-        while True:
-            rval = session.get(bash_url + '?start=5000#last', headers=headers)
-            # 检验是否需要输入验证码?
-            import re
-            ##寻找要提交的ck参数
-            data['ck'] = re.search('ck=(\w+)', rval.text).group(1)
+        rval = session.get(bash_url + '?start=5000#last', headers=headers)
+        # 检验是否需要输入验证码?
+        ##寻找要提交的ck参数
+        data['ck'] = re.search('ck=(\w+)', rval.text).group(1)
 
-            captcha_need = re.search('src=\"(.*captcha.*)+\"', rval.text)
-            # 需要验证输入验证码
-            if captcha_need != None:
-                mat = re.search('src=\"(.*captcha.*)+\"', rval.text).group(1).split(' ')
-                # 验证码url
-                img_url = mat[0]
-                # 验证码隐藏域
-                captcha_id = re.split('&', img_url)[0].split('=')[1]
-                if mat:
-                    with open('code.jpg', 'wb') as f:
-                        f.write(requests.get(img_url).content)
-                    try:
-                        img = Image.open('code.jpg')
-                        img.show()
-                        img.close()
-                    except:
-                        print('图片打开失败')
-                    else:
-                        veri_code_input = input('输入验证码:')
-                        # 构造验证码提交的数据
-                        ding_text = requests.get('http://whatthecommit.com/index.txt').text
-                        print(ding_text)
-                        data_captcha = {
-                            "rv_comment": ding_text,
-                            "ck": data['ck'],
-                            'start': '0',
-                            'submit_btn': '加上去',
-                            'captcha-solution': veri_code_input,
-                            'captcha-id': captcha_id
-                        }
-                        session.post(url=url, data=data_captcha, headers=headers)
-                        break
+        captcha_need = re.search('src=\"(.*captcha.*)+\"', rval.text)
+        # 需要验证输入验证码
+        if captcha_need != None:
+            mat = re.search('src=\"(.*captcha.*)+\"', rval.text).group(1).split(' ')
+            # 验证码url
+            img_url = mat[0]
+            # 验证码隐藏域
+            captcha_id = re.split('&', img_url)[0].split('=')[1]
+            if mat:
+                with open('code.jpg', 'wb') as f:
+                    f.write(requests.get(img_url).content)
+                try:
+                    img = Image.open('code.jpg')
+                    img.show()
+                    img.close()
+                except:
+                    print('图片打开失败')
+                else:
+                    veri_code_input = input('输入验证码:')
+                    # 构造验证码提交的数据
+                    ding_text = requests.get('http://whatthecommit.com/index.txt').text
+                    print(ding_text)
+                    data_captcha = {
+                        "rv_comment": ding_text,
+                        "ck": data['ck'],
+                        'start': '0',
+                        'submit_btn': '加上去',
+                        'captcha-solution': veri_code_input,
+                        'captcha-id': captcha_id
+                    }
+                    session.post(url=url, data=data_captcha, headers=headers)
+                    break
 
-            # 不需要验证码的时候，直接提交
-            else:
-                session.post(url=url, data=data, headers=headers)
-                break
-        print('等待20秒')
-        time.sleep(20)
+        # 不需要验证码的时候，直接提交
+        else:
+            session.post(url=url, data=data, headers=headers)
+            break
 
 if __name__ == '__main__':
     douban_session = login(
         username='',
         passwd=''
     )
-    join_the_group(douban_session)
-    replies(douban_session)
+    for bash_url in bash_urls:
+        print('正在加入%s的小组'%bash_url)
+        join_the_group(douban_session,bash_url)
+        time.sleep(1)
+    while True:
+        for url in bash_urls:
+            print('正在回帖%s')
+            replies(douban_session,url)
+            print('等待五秒，回帖下一个URL')
+            time.sleep(5)
+        print('等待300秒开始下一轮回帖！')
+        time.sleep(300)
